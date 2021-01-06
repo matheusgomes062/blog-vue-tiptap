4cdcb6b983d860e71d8a8c830900dd24809475e6
Matheus Gomes <matheusgomes062@gmail.com>
2021-01-06 02:01:03 -0300

    feat(blog): adicionado tiptap no projeto
    Agora é possível editar o texto

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,7 +1,171 @@
-<template> <div></div></template>
+<template>
+  <div>
+    <editor-menu-bar :editor="editor" v-slot="{ commands, isActive }">
+      <div>
+        <button
+          :class="{ 'is-active': isActive.bold() }"
+          @click="commands.bold"
+        >
+          Bold
+        </button>
+        <button
+          :class="{ 'is-active': isActive.italic() }"
+          @click="commands.italic"
+        >
+          Italic
+        </button>
+
+        <button
+          :class="{ 'is-active': isActive.strike() }"
+          @click="commands.strike"
+        >
+          Strike
+        </button>
+
+        <button
+          :class="{ 'is-active': isActive.underline() }"
+          @click="commands.underline"
+        >
+          underline
+        </button>
+
+        <button
+          :class="{ 'is-active': isActive.code() }"
+          @click="commands.code"
+        >
+          code
+        </button>
+
+        <button
+          :class="{ 'is-active': isActive.paragraph() }"
+          @click="commands.paragraph"
+        >
+          paragraph
+        </button>
+
+        <button
+          :class="{ 'is-active': isActive.heading({ level: 1 }) }"
+          @click="commands.heading({ level: 1 })"
+        >
+          H1
+        </button>
+
+        <button
+          :class="{ 'is-active': isActive.heading({ level: 2 }) }"
+          @click="commands.heading({ level: 2 })"
+        >
+          H2
+        </button>
+
+        <button
+          :class="{ 'is-active': isActive.heading({ level: 3 }) }"
+          @click="commands.heading({ level: 3 })"
+        >
+          H3
+        </button>
+
+        <button
+          :class="{ 'is-active': isActive.bullet_list() }"
+          @click="commands.bullet_list"
+        >
+          ul
+        </button>
+
+        <button
+          :class="{ 'is-active': isActive.ordered_list() }"
+          @click="commands.ordered_list"
+        >
+          ol
+        </button>
+
+        <button
+          :class="{ 'is-active': isActive.blockquote() }"
+          @click="commands.blockquote"
+        >
+          quote
+        </button>
+
+        <button
+          :class="{ 'is-active': isActive.code_block() }"
+          @click="commands.code_block"
+        >
+          code
+        </button>
+
+        <button @click="commands.horizontal_rule">
+          hr
+        </button>
+
+        <button @click="commands.undo">
+          undo
+        </button>
+
+        <button @click="commands.redo">
+          redo
+        </button>
+      </div>
+    </editor-menu-bar>
+    <editor-content :editor="editor" />
+  </div>
+</template>
 
 <script>
-export default {};
-</script>
+import { Editor, EditorContent, EditorMenuBar } from "tiptap";
+import {
+  Blockquote,
+  CodeBlock,
+  HardBreak,
+  Heading,
+  OrderedList,
+  BulletList,
+  ListItem,
+  TodoItem,
+  TodoList,
+  Bold,
+  Code,
+  Italic,
+  Link,
+  Strike,
+  Underline,
+  History,
+} from "tiptap-extensions";
 
-<style></style>
+export default {
+  name: "Home",
+  components: {
+    EditorMenuBar,
+    EditorContent,
+  },
+  data() {
+    return {
+      editor: new Editor({
+        extensions: [
+          new Blockquote(),
+          new CodeBlock(),
+          new HardBreak(),
+          new Heading({ levels: [1, 2, 3] }),
+          new BulletList(),
+          new OrderedList(),
+          new ListItem(),
+          new TodoItem(),
+          new TodoList(),
+          new Bold(),
+          new Code(),
+          new Italic(),
+          new Link(),
+          new Strike(),
+          new Underline(),
+          new History(),
+        ],
+        content: `
+          <h1>Yay Headlines!</h1>
+          <p>All these <strong>cool tags</strong> are working now.</p>
+        `,
+      }),
+    };
+  },
+  beforeDestroy() {
+    this.editor.destroy();
+  },
+};
+</script>
